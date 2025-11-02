d365963139b31a2b99d07f28461365c72c4e6585
Mihajlo <zirafica.98@gmail.com>
2025-11-02 15:42:50 +0100

    add port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://ipsapp.duckdns.org/api'
+  apiUrl: 'https://ipsapp.duckdns.org:5002/api'
   //apiUrl: 'http://localhost:5050/api' // za lokalni razvoj
 };
