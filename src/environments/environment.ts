7bd88e920d85e3dfd0fb2afe00dd1cff2ad52dcb
Mihajlo <zirafica.98@gmail.com>
2025-11-03 19:23:16 +0100

    Add server info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://ipsapp.duckdns.org/api',
+  apiUrl: 'https://ipsapp.duckdns.org/api',
   // Placeholder podaci primaoca (možeš kasnije prilagoditi realnim vrednostima)
     receiverName: 'PILE S.Z.A.R',
   receiverAccount: '190-0000000141610-17',
     receiverAddress: 'Ramska 7, 11000 Beograd',
     receiverEmail: 'autoservispile2@gmail.com',
     receiverPIB: '100024510',
-  receiverMB: '54145560',
-  apiUrl: 'http://localhost:5002/api' // za lokalni razvoj
+  receiverMB: '54145560'
+  //apiUrl: 'http://localhost:5002/api' // za lokalni razvoj
 };
